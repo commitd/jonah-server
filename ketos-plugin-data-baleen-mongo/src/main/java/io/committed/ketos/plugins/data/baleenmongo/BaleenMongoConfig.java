8259c5320d1d398999765c3ddde8d2c48a35f556
Chris Flatley <chris@committed.io>
2017-10-17 17:40:18 +0100

    Add plugin extension to the baleen mongo module

---
+++ ketos-plugin-data-baleen-mongo/src/main/java/io/committed/ketos/plugins/data/baleenmongo/BaleenMongoConfig.java
@@ -0,0 +1,7 @@
+package io.committed.ketos.plugins.data.baleenmongo;
+
+@Configuration
+@ComponentScan(basePackages = { BaleenMongoConfig.class })
+public class BaleenMongoConfig {
+
+}
---
+++ ketos-plugin-data-baleen-mongo/src/main/java/io/committed/ketos/plugins/data/baleenmongo/BaleenMongoPlugin.java
@@ -0,0 +1,7 @@
+package io.committed.ketos.plugins.data.baleenmongo;
+
+import io.committed.vessel.extensions.VesselGraphQlExtension;
+
+public class BaleenMongoPlugin implements VesselGraphQlExtension {
+
+}
