8c0787023dd465926373145d20dfbb7e17bc3f31
Chris Flatley <chris@committed.io>
2017-10-26 22:28:45 +0200

    Create an example corpus provider bean (hack)

--- ketos-plugin-data-baleen-mongo/src/main/java/io/committed/ketos/plugins/data/baleenmongo/BaleenMongoConfig.java
+++ ketos-plugin-data-baleen-mongo/src/main/java/io/committed/ketos/plugins/data/baleenmongo/BaleenMongoConfig.java
@@ -1,14 +1,29 @@
 package io.committed.ketos.plugins.data.baleenmongo;
 
+import java.util.HashMap;
+import java.util.List;
+import java.util.Map;
+
+import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 import org.springframework.data.mongodb.repository.config.EnableReactiveMongoRepositories;
 
+import io.committed.ketos.plugins.data.baleen.BaleenCorpus;
 import io.committed.ketos.plugins.data.baleenmongo.repository.BaleenDocumentRepository;
+import io.committed.ketos.plugins.graphql.baleenservices.providers.DataProvider;
+import io.committed.ketos.plugins.providers.services.CorpusProviders;
 
 @Configuration
 @ComponentScan(basePackageClasses = { BaleenMongoConfig.class })
 @EnableReactiveMongoRepositories(basePackageClasses = { BaleenDocumentRepository.class })
 public class BaleenMongoConfig {
 
+  // TODO: Hack until we have proper corpus provider implementation
+  @Bean
+  public CorpusProviders corpusProviders(final List<DataProvider> providers) {
+    final Map<BaleenCorpus, List<DataProvider>> map = new HashMap<>();
+    map.put(new BaleenCorpus("baleen", "Mongo Baleen"), providers);
+    return new CorpusProviders(map);
+  }
 }
