1c5da4dd46d8bf6e20e9fdb7796cad0560bc88c8
Chris Flatley <chris@committed.io>
2017-11-21 16:14:22 +0000

    Example plugin using only basic html

---
+++ example-ui-html/src/main/java/io/committed/ketos/example/ui/html/ExampleHtmlUi.java
@@ -0,0 +1,11 @@
+package io.committed.ketos.example.ui.html;
+
+import io.committed.vessel.extensions.VesselUiExtension;
+
+public class ExampleHtmlUi implements VesselUiExtension {
+
+  @Override
+  public String getId() {
+    return "example-html";
+  }
+}
