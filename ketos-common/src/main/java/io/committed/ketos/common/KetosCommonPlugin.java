fa6bef3b7a0992e974d4728004a4b8c5a9e8d8c1
Chris Flatley <chris@committed.io>
2017-11-24 08:42:49 +0000

    Create ketos core plugin and add to build script

---
+++ ketos-common/src/main/java/io/committed/ketos/common/KetosCommonPlugin.java
@@ -0,0 +1,10 @@
+package io.committed.ketos.common;
+
+import org.springframework.context.annotation.Configuration;
+
+import io.committed.vessel.extensions.VesselExtension;
+
+@Configuration
+public class KetosCommonPlugin implements VesselExtension {
+
+}
