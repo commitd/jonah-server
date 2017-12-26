24a3c0b40ed6706866dfce8f25ce54f0c33a8518
Chris Flatley <chris@committed.io>
2017-12-26 11:38:12 +0100

    Add configuration annotation to extension

--- ketos-ui-example-html/src/main/java/io/committed/ketos/example/ui/html/ExampleHtmlUi.java
+++ ketos-ui-example-html/src/main/java/io/committed/ketos/example/ui/html/ExampleHtmlUi.java
@@ -1,7 +1,10 @@
 package io.committed.ketos.example.ui.html;
 
+import org.springframework.context.annotation.Configuration;
+
 import io.committed.vessel.extensions.VesselUiExtension;
 
+@Configuration
 public class ExampleHtmlUi implements VesselUiExtension {
 
   @Override
