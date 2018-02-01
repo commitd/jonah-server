346b709cbd6618f3d548bc44db974721f7c27ed2
Chris Flatley <chris@committed.io>
2018-02-01 14:56:43 +0000

    Add provider selection to dataset drop down

--- ketos-graphql-baleen/src/main/java/io/committed/ketos/graphql/baleen/root/CorpusService.java
+++ ketos-graphql-baleen/src/main/java/io/committed/ketos/graphql/baleen/root/CorpusService.java
@@ -4,6 +4,7 @@ import org.springframework.beans.factory.annotation.Autowired;
 import io.committed.invest.extensions.annotations.GraphQLService;
 import io.committed.invest.extensions.data.dataset.Dataset;
 import io.committed.invest.extensions.data.dataset.DatasetRegistry;
+import io.committed.invest.extensions.data.providers.DataProviders;
 import io.committed.ketos.common.data.BaleenCorpus;
 import io.leangen.graphql.annotations.GraphQLArgument;
 import io.leangen.graphql.annotations.GraphQLNonNull;
@@ -16,9 +17,12 @@ public class CorpusService {
 
   private final DatasetRegistry corpusRegistry;
 
+  private final DataProviders corpusProviders;
+
   @Autowired
-  public CorpusService(final DatasetRegistry corpusRegistry) {
+  public CorpusService(final DatasetRegistry corpusRegistry, final DataProviders corpusProviders) {
     this.corpusRegistry = corpusRegistry;
+    this.corpusProviders = corpusProviders;
 
   }
 
@@ -29,8 +33,25 @@ public class CorpusService {
   }
 
   @GraphQLQuery(name = "corpora", description = "Access to all corpora available")
-  public Flux<BaleenCorpus> corpora() {
-    return corpusRegistry.getDatasets().map(this::toBaleenCorpus);
+  public Flux<BaleenCorpus> corpora(
+      @GraphQLArgument(name = "provider") final String provider,
+      @GraphQLArgument(name = "database") final String database,
+      @GraphQLArgument(name = "datasource") final String datasource) {
+    Flux<Dataset> flux = corpusRegistry.getDatasets();
+    if (provider != null || database != null || datasource != null) {
+      flux = flux.filterWhen(d -> filter(d, provider, database, datasource));
+
+    }
+    return flux.map(this::toBaleenCorpus);
+  }
+
+  private Mono<Boolean> filter(final Dataset dataset, final String provider, final String database,
+      final String datasource) {
+
+    return corpusProviders.findForDataset(dataset.getId())
+        .any(p -> (provider == null || provider.equals(p.getProviderType()))
+            && (database == null || database.equals(p.getDatabase()))
+            && (datasource == null || datasource.equals(p.getDatasource())));
   }
 
   private BaleenCorpus toBaleenCorpus(final Dataset corpus) {
