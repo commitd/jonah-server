131d7b36c7e9ac68e86259e9c56caccf802620c6
Chris Flatley <chris@committed.io>
2018-02-14 16:17:23 +0100

    Ensure correct parent is set for BaleenRelation

--- ketos-common/src/main/java/io/committed/ketos/common/data/BaleenRelation.java
+++ ketos-common/src/main/java/io/committed/ketos/common/data/BaleenRelation.java
@@ -1,15 +1,14 @@
 package io.committed.ketos.common.data;
 
+import java.util.Collections;
 import java.util.Map;
 import io.committed.ketos.common.graphql.support.AbstractGraphQLNode;
 import io.leangen.graphql.annotations.GraphQLId;
-import lombok.AllArgsConstructor;
 import lombok.Builder;
 import lombok.Data;
 import lombok.EqualsAndHashCode;
 
 @Data
-@AllArgsConstructor
 @EqualsAndHashCode(callSuper = true)
 @Builder
 public class BaleenRelation extends AbstractGraphQLNode {
@@ -34,5 +33,31 @@ public class BaleenRelation extends AbstractGraphQLNode {
   private final BaleenMention target;
 
   private final Map<String, Object> properties;
+
+  public BaleenRelation(final String id, final String docId, final int begin, final int end,
+      final String type, final String subtype,
+      final String value, final BaleenMention source, final BaleenMention target,
+      final Map<String, Object> properties) {
+    super();
+    this.id = id;
+    this.docId = docId;
+    this.begin = begin;
+    this.end = end;
+    this.type = type;
+    this.subtype = subtype;
+    this.value = value;
+    this.source = source;
+    this.target = target;
+    this.properties = properties == null ? Collections.emptyMap() : properties;
+
+    // Ensure the correctness of the source and target
+
+    if (this.source != null) {
+      this.source.setParent(this);
+    }
+    if (this.target != null) {
+      this.target.setParent(this);
+    }
+  }
 
 }
