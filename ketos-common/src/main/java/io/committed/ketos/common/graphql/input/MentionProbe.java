e7ebcfb4e7fde2c4168b3424888328a3d7ae57c1
Chris Flatley <chris@committed.io>
2018-02-19 16:06:33 +0100

    Add subType to mention filter

--- ketos-common/src/main/java/io/committed/ketos/common/graphql/input/MentionProbe.java
+++ ketos-common/src/main/java/io/committed/ketos/common/graphql/input/MentionProbe.java
@@ -8,10 +8,10 @@ public class MentionProbe {
 
   private String docId;
   private String id;
-  private Double confidence;
   private Integer begin;
   private Integer end;
   private String type;
+  private String subType;
   private String value;
   private String entityId;
   private Map<String, Object> properties;
@@ -22,6 +22,7 @@ public class MentionProbe {
     filter.setDocId(docId);
     filter.setId(id);
     filter.setType(type);
+    filter.setSubType(subType);
     filter.setValue(value);
     filter.setProperties(properties);
     filter.setEntityId(entityId);
