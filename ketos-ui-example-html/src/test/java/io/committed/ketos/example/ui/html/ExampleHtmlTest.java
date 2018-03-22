5baac67ce14c6d2d3e7eff6142bee2f63aa16d8a
Chris Flatley <chris@committed.io>
2018-03-22 20:49:32 +0000

    Add webflux test for example html

---
+++ ketos-ui-example-html/src/test/java/io/committed/ketos/example/ui/html/ExampleHtmlTest.java
@@ -0,0 +1,32 @@
+package io.committed.ketos.example.ui.html;
+
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.test.autoconfigure.web.reactive.WebFluxTest;
+import org.springframework.test.annotation.DirtiesContext;
+import org.springframework.test.context.ContextConfiguration;
+import org.springframework.test.context.junit4.SpringRunner;
+import org.springframework.test.web.reactive.server.WebTestClient;
+import io.committed.invest.server.core.ServerCoreConfiguration;
+import io.committed.invest.test.InvestTestContext;
+
+
+@RunWith(SpringRunner.class)
+@WebFluxTest
+@ContextConfiguration(classes = {InvestTestContext.class, ExampleHtmlUi.class, ServerCoreConfiguration.class})
+@DirtiesContext
+public class InvestUiApplicationExtensionTest {
+
+  @Autowired
+  private WebTestClient webClient;
+
+  @Test
+  public void getIndex() {
+    this.webClient.get()
+        .uri("/ui/example-html/index.html")
+        .exchange()
+        .expectStatus().is2xxSuccessful();
+  }
+
+}
