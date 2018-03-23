e0a624d28bf89d06921aeb25ab27008e6428146e
Steven Taylor <steven@committed.io>
2018-03-23 18:30:04 +0000

    Fixes data issue in crud provider test

--- ketos-data-baleen-mongo/src/test/java/io/committed/ketos/plugins/data/mongo/providers/MongoCrudDocumentProviderTest.java
+++ ketos-data-baleen-mongo/src/test/java/io/committed/ketos/plugins/data/mongo/providers/MongoCrudDocumentProviderTest.java
@@ -37,13 +37,13 @@ public class MongoCrudDocumentProviderTest
   @Override
   public BaleenDocumentReference getDeleteReference() {
     BaleenDocumentReference ref = new BaleenDocumentReference();
-    ref.setDocumentId("575f6e573aaa400bd69f6c282ced6c81969aff20abe96be4ac8989f1f74ef55b");
+    ref.setDocumentId("a19f6ed4-87bb-4dc6-919e-596761127082");
     return ref;
   }
 
   @Override
   public BaleenDocument getSaveItem() {
-    BaleenDocument document = new BaleenDocument("fd683817e7b32a1beae34d5a166f26d78830973c27a07eec7877760711993add",
+    BaleenDocument document = new BaleenDocument("cffa7e3a-4664-4308-b97b-cb7a1f96e7ff",
         Collections.EMPTY_LIST, "", new PropertiesMap());
     return document;
   }
